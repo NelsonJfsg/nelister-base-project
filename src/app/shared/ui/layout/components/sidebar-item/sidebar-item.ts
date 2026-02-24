c82fd35af06473779588d836eb0a0b52601e9c2d
NelsonJfsg <nelin09001@gmail.com>
2026-02-24 12:16:12 -0700

    Added better experience in mobile ui

--- src/app/shared/ui/layout/components/sidebar-item/sidebar-item.ts
+++ src/app/shared/ui/layout/components/sidebar-item/sidebar-item.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, WritableSignal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NavItem } from '../../interfaces/nav-item.interface';
 import { Router } from '@angular/router';
@@ -17,8 +17,11 @@ export class SidebarItem {
   private router = inject(Router);
   private navbarService = inject(NavbarService);
 
-  @Input() isCollapsed : any;
+  @Input() isCollapsed !: WritableSignal<boolean>;
 
+  private isMobile () : boolean {
+    return window.innerWidth <= 768;
+  }
   @Input() navItem : NavItem = {
     id : 0,
     title : '',
@@ -30,6 +33,10 @@ export class SidebarItem {
     
     if (this.navItem.action) {
       this.navItem.action();
+    }
+
+    if (this.isMobile()) {
+      this.isCollapsed.update(() => true);
     }
 
     this.router.navigate([this.navItem.routeLink]);
